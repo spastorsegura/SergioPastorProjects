f41caed95887f9e56d9763947b12958627615893
spastorsegura <67179586+spastorsegura@users.noreply.github.com>
2025-12-22 10:20:59 -0500

    actualización: pattern

--- components/Pattern.jsx
+++ components/Pattern.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Pattern = () => {
   return (
-    <div className=" md:flex absolute left-24 top-0 w-full h-full bg-[url(/assets/pattern.svg))] bg-no-repeat bg-cover bg-center xl:bg-contain xl:-top-[110px] -z-10 opacity-20"></div>
+    <div className=" md:flex absolute xl:left-24 top-0 w-full h-full bg-[url(/assets/pattern.svg))] bg-no-repeat bg-cover bg-center xl:bg-contain xl:-top-[110px] -z-10 opacity-20"></div>
   );
 };
 
